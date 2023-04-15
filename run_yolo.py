011fdac53e17cd15df427aded8a5cc20aacb11c1
schelli <mschellb4ch@gmail.com>
2023-04-15 17:46:17 +0200

    moved a lot of code to base, introduction of 320x320 models

--- run_yolo.py
+++ run_yolo.py
@@ -1,171 +1,45 @@
 import sys
-import time
 import ctypes
 import argparse
-from threading import Thread
 
-import cv2
 import torch
-from ultralytics import YOLO
-import pyautogui
-import numpy as np
 from rich import print
+from ultralytics import YOLO
 
-from src.utils import accurate_timing
 from src.base import AimAide
 
 
 class AimAideYolo(AimAide):
-    def __init__(self, screensz, sectionsz, grabber, weights):
+    def __init__(self, screensz, sectionsz, grabber, model, side):
         if grabber == 'd3d_gpu':
-            print('d3d_gpu grabber is only available for AimAide with TensorRT, switching to d3d_np!')
+            print('[yellow]d3d_gpu grabber is only available for AimAide with TensorRT, switching to d3d_np!')
             grabber = 'd3d_np'
-        super().__init__(screensz, sectionsz, grabber)
+        
+        super().__init__(screensz, sectionsz, grabber, side)
 
         if torch.cuda.is_available():
-            print('CUDA device:', torch.cuda.get_device_name(0))
-        
+            print('[green]CUDA device found:', torch.cuda.get_device_name(0))
         else:
-            print('No CUDA device found.')
+            print('[red]No CUDA device found.')
             sys.exit(0)
 
-        self.model = YOLO(weights)
-        print(f'Weights: {weights}')
+        if model.endswith('engine'):
+            print('[yellow]Specified TensorRT engine when YOLO.pt is needed...Loading YOLO weights!')
+            model = model.replace('engine', 'pt')
 
-        self._grabber = grabber
+        self.model = YOLO(model)
+        print(f'[green]Weights loaded:[/green] {model}')
 
-    def benchmark(self, iters=300):
-        t1 = time.perf_counter()
-        while time.perf_counter() - t1 < 5:
-            t = time.perf_counter() - t1
-            print(f'Running in benchmark mode for {iters} iterations in {(4-t):.1f} sec!', end='\r', flush=True)
-            time.sleep(1)
-        print('\n')
-        self._avg_fps = []
-        self.run(False, side='dm', minconf=.8, sensitivity=1, visualize=False,
-                 view_only=True ,benchmark_mode=True, benchmark_iters=iters)
-
-
-    def run(self, prefer_body, side, minconf, sensitivity, visualize, view_only, 
-            benchmark_mode=False, benchmark_iters=-1):
-            
-        count = 0
-        count_fps = 0
-        dx, dy = 0, 0
-        self.conf = 0
-
-        self._switch_side(side)
-        self.listener_switch = Thread(target=self.user_switch_side, daemon=True)
-        self.listener_switch.start()
-
-        while True:
-            runtime_start = time.perf_counter()
-
-            if self._grabber == 'win32':
-                img = self._grab()
-
-            if self._grabber == 'd3d_np':
-                img = self._grab_d3d_np()
-
-            results = self.model.predict(img, device=0, verbose=False, conf=minconf, max_det=10)
-
-            bboxes = []
-            confs = []
-            labels = []
-            for d in reversed(results[0].boxes):
-                bboxes.append(d.xywh.squeeze().cpu().numpy().astype(int).tolist())
-                confs.append(d.conf.squeeze().cpu().numpy().tolist())
-                labels.append(int(d.cls.squeeze().cpu()))
-        
-            #...result in uniform data format
-            bboxes = np.array(bboxes, dtype=np.int16)
-            confs = np.array(confs, dtype=np.float32)
-            labels = np.array(labels, np.uint8)
-
-            if bboxes.ndim == 1:
-                bboxes = bboxes[None, :]
-
-            if self.side == 'ct':
-                valid_idcs = np.where(labels>1)[0]
-                bboxes = bboxes[valid_idcs]
-                confs = confs[valid_idcs]
-                labels = labels[valid_idcs]
-
-            if self.side == 't':
-                valid_idcs = np.where(labels<=1)[0]
-                bboxes = bboxes[valid_idcs]
-                confs = confs[valid_idcs]
-                labels = labels[valid_idcs]
-                
-            if labels.size > 1:
-                dist = bboxes[:, 0] - self.section_size // 2
-                right_sided = np.where(dist>0)[0]
-                if len(right_sided) > 0:
-                    dist = dist[right_sided]
-                    bboxes = bboxes[right_sided]
-                    confs = confs[right_sided]
-                    labels = labels[right_sided]             
-                closest = np.argsort(dist)
-   
-                if (0 or 2 in labels) and (prefer_body):
-                    closest_body_idx = np.where((labels[closest] == 0).any() or (labels[closest] == 2).any())[0][0]
-                    target_body_idx = closest[closest_body_idx]
-                    target= bboxes[target_body_idx]
-                    self.conf = float(confs[target_body_idx])
-                else:
-                    target = bboxes[closest[0]]
-                    self.conf = float(confs[closest[0]])
-                
-            elif labels.size == 1:
-                target = bboxes
-                self.conf = float(confs)
-            else:
-                self.conf = float(0)
-
-            if labels.size > 0:
-                cx, cy, w, h = np.squeeze(target)
-                x1, x2 = (cx - w//2 - self.section_size // 2, cx + w//2 - self.section_size // 2)
-                y1, y2 = (cy - h//2 - self.section_size // 2, cx + h//2  - self.section_size // 2)
-                dx = int(cx-self.section_size//2)
-                dy = int(cy-self.section_size//2)
-                self.detected = True
-            else:
-                self.detected = False 
-
-            if self.detected and self.conf > minconf and np.hypot(dx, dy) < self.section_size//4 and not view_only:
-                self._smooth_linear_aim(dx, dy, [x1, y1, x2, y2], sensitivity)
-            
-            if visualize:
-                plot = self.visualize(img, bboxes, confs, labels, minconf)
-                cv2.imshow('cap', plot)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-
-            runtime_end = time.perf_counter()        
-            if count_fps == 30:
-                print(f'[{self._side_color}]FPS:{(int(1/(runtime_end-runtime_start)))} | conf:{self.conf:.2f} dx:{abs(dx):03d} dy:{abs(dy):03d}', end='\r', flush=True)
-                count_fps = 0
-            else:
-                count_fps += 1
-
-            if benchmark_mode:
-                count += 1
-                self._avg_fps.append(int(1/(runtime_end-runtime_start)))
-                if count > benchmark_iters:
-                    print(f'Average FPS: {np.mean(np.array(self._avg_fps)):.2f} (dev:{np.std(np.array(self._avg_fps)):.2f})                       _')
-                    print('Above 60 FPS increase sensitivity if aim aiding feels unnatural (-sensitivity argument: default 1).')
-                    break
-        sys.exit()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='AimAide with Ultralytics YOLO', description='Run AimAide with Ultralytics YOLO')
-    parser.add_argument('--input_size', type=int, default=640,  
+    parser.add_argument('--input_size', type=int, default=320,  
                         help='dimension of the input image for the detector')
     parser.add_argument('--grabber', type=str, default='win32', help='selected grabber (win32, d3d_gpu, d3d_np)')
-    parser.add_argument('--weights', default='models\yolov8s_csgo_mirage-640-v5-al-gen-bg.pt', 
+    parser.add_argument('--model', default='models\yolov8s_csgo_mirage-320-v41-al-gen-bg.pt', 
                         help='selected weights (YOLO) ')
     parser.add_argument('--side', type=str, default='dm', help='which side your are on, ct, t or dm (deathmatch))')
-    parser.add_argument('--minconf', type=float, default=0.8, help='minimum detection confidence')
+    parser.add_argument('--minconf', type=float, default=0.7, help='minimum detection confidence')
     parser.add_argument('--sensitivity' , type=int, default=1, 
                         help='sensitivity mode, increase when having a high framerate or chaotic aim')
     parser.add_argument('--visualize', action='store_true', help='show live detector output in a new window')
@@ -174,14 +48,14 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     w, h = ctypes.windll.user32.GetSystemMetrics(0), ctypes.windll.user32.GetSystemMetrics(1)
-    Aim = AimAideYolo((w, h), args.input_size, args.grabber, args.weights)
+    Aim = AimAideYolo((w, h), args.input_size, args.grabber, args.model, args.side)
 
     if args.benchmark:
-        Aim.benchmark()
+        Aim._benchmark('yolo')
     else:
-        Aim.run(False, args.side, args.minconf, args.sensitivity, args.visualize, args.view_only)
+        Aim.run('yolo', args.minconf, args.sensitivity, args.visualize, False, args.view_only, args.benchmark)
 
     Aim.listener_switch.join()
 
-    if not args.grabber == 'win32':
+    if not Aim._grabber == 'win32':
         Aim.d.stop()
--- src/base.py
+++ src/base.py
@@ -1,28 +1,35 @@
 global d3d_err_flag
 
 import sys
-import cv2
-import numpy as np
+import time
+from typing import Union
+from threading import Thread
+
 try:
     import d3dshot
     d3d_err_flag = False
 except ImportError as e:
     print(e, '...running win32 grabber')
     d3d_err_flag = True
-import torch
-import pyautogui
-import win32gui, win32ui, win32con
-from typing import Union
-from .utils import accurate_timing
 
+import cv2
+import torch 
+import pyautogui
+import numpy as np
+from rich import print
+import win32gui, win32ui, win32con
+from torchvision.ops import box_convert
+
+from .utils import accurate_timing
+from .utils_trt import TRTModule, blob, letterbox, det_postprocess
 
 MOUSE_X_MOVE_DAMPING = 1
 MOUSE_Y_MOVE_DAMPING = 1.3
 
 
 class AimAide():
-    def __init__(self, screensz: int, sectionsz: int, grabber: str) -> None:        
-        self.side = 'dm'
+    def __init__(self, screensz: int, sectionsz: int, grabber: str, side: str) -> None:        
+        self.side = side
         if self.side == 'dm':
             self._side_color = 'green'
         if self.side == 'ct':
@@ -36,25 +43,29 @@ class AimAide():
         self.center_x = self.screen_width // 2
         self.center_y = self.screen_height // 2
         
-        if grabber == 'd3d_gpu' and not d3d_err_flag:
-            self._grabber = grabber
-            print(f'Starting grabber: {self._grabber}')
-            self.d = d3dshot.create(capture_output="pytorch_gpu")
-            self.d.display = self.d.displays[0]
+        try:
+            if grabber == 'd3d_gpu' and not d3d_err_flag:
+                self._grabber = 'd3d_gpu'
+                self.d = d3dshot.create(capture_output="pytorch_gpu")
+                self.d.display = self.d.displays[0]
 
-        if grabber == 'd3d_np' and not d3d_err_flag:
-            self._grabber = grabber
-            print(f'Starting grabber: {self._grabber}')
-            self.d = d3dshot.create(capture_output="numpy")
-            self.d.display = self.d.displays[0]
+            if grabber == 'd3d_np' and not d3d_err_flag:
+                self._grabber = 'd3d_np'
+                self.d = d3dshot.create(capture_output="numpy")
+                self.d.display = self.d.displays[0]
 
-        if grabber == 'win32' or d3d_err_flag:
-            self._grabber = 'win32'
-            print(f'Starting grabber: {self._grabber}')
-            self.dcObj = win32ui.CreateDCFromHandle(win32gui.GetWindowDC(win32gui.GetDesktopWindow()))
-            self.cDC = self.dcObj.CreateCompatibleDC()
-            self.dataBitMap = win32ui.CreateBitmap()
-            self.dataBitMap.CreateCompatibleBitmap(self.dcObj, self.section_size, self.section_size)
+            if grabber == 'win32' or d3d_err_flag:
+                self._grabber = 'win32'
+                self.dcObj = win32ui.CreateDCFromHandle(win32gui.GetWindowDC(win32gui.GetDesktopWindow()))
+                self.cDC = self.dcObj.CreateCompatibleDC()
+                self.dataBitMap = win32ui.CreateBitmap()
+                self.dataBitMap.CreateCompatibleBitmap(self.dcObj, self.section_size, self.section_size)
+                self.cDC.SelectObject(self.dataBitMap)
+        except Exception as e:
+            print(e)
+            sys.exit()
+                   
+        print(f'[green]Grabber started:[/green] {self._grabber}')
 
     def _switch_side(self, side: str) -> None:
         self.side = side
@@ -71,21 +82,8 @@ class AimAide():
             self._switch_side(self.side)
             print(end='\n')
 
-    def perform_target_acquisition(self):
-        pass
-
-    def processing_trt_tensor(self) -> tuple[np.ndarray, np.ndarray, float]:
-        pass
-
-    def processing_trt(self) -> tuple[np.ndarray, np.ndarray, float]:
-        pass
-
-    def processing_yolo(self) -> tuple[np.ndarray, np.ndarray, float]:
-        pass
-
     def _grab(self) -> np.ndarray:
         try: 
-            self.cDC.SelectObject(self.dataBitMap)
             self.cDC.BitBlt((0, 0), (self.section_size, self.section_size), self.dcObj, 
                             (self.center_x-self.section_size//2, self.center_y-self.section_size//2), 
                             win32con.SRCCOPY)
@@ -106,9 +104,10 @@ class AimAide():
                                                self.screen_height//2 - self.section_size//2, 
                                                self.screen_width//2 + self.section_size//2, 
                                                self.screen_height//2 + self.section_size//2)
-                                )
+                                       )
             tensor = torch.flip(tensor, dims=(2, )).permute(2, 0, 1)[None, :, :, :]
-            return tensor.type(torch.float32) / 255
+            
+            return tensor.type(torch.float16) / 255
 
         except Exception as e:
             print(e)
@@ -119,33 +118,157 @@ class AimAide():
                                             self.screen_height//2 - self.section_size//2, 
                                             self.screen_width//2 + self.section_size//2, 
                                             self.screen_height//2 + self.section_size//2)
-                                             )
+                                    )
             
             return img
 
-    def _smooth_linear_aim(self, dx: int, dy: int, xyxy: list, sensitivity: int) -> None:
-        x1, y1, x2, y2, = xyxy
-        n_steps = abs(dx//6) if abs(dx) > 100 else abs(dx//8)
-        if n_steps <= 4:
-            n_steps = 4
-        
-        for _ in range(n_steps):
-            smooth = 2 * n_steps * sensitivity
-            if (x1//4 > 0) or (x2//4 < 0) or (y1//8 > 0) or (y2//8 < 0):
-                pyautogui.move(int(dx//smooth/MOUSE_X_MOVE_DAMPING), int(dy//smooth/MOUSE_Y_MOVE_DAMPING), 0, _pause=False)
-            _ = accurate_timing(sensitivity)
- 
-    def visualize(self, frame: np.ndarray, 
-                        bboxes: Union[np.ndarray, list], 
-                        confs: Union[np.ndarray, list], 
-                        labels: Union[np.ndarray, list], 
-                        minconf: float) -> np.ndarray:
+    def _inference_yolo(self):
+        if self._grabber == 'win32':
+            img = self._grab()
 
-        frame =  np.ascontiguousarray(frame, dtype=np.uint8)
+        if self._grabber == 'd3d_np':
+            img = self._grab_d3d_np()
+
+        results = self.model.predict(img, device=0, verbose=False, max_det=10)
+
+        bboxes, confs, labels = [], [], []
+        for d in reversed(results[0].boxes):
+            bboxes.append(d.xywh.squeeze().cpu().numpy().astype(int).tolist())
+            confs.append(d.conf.squeeze().cpu().numpy().tolist())
+            labels.append(int(d.cls.squeeze().cpu()))
+    
+        bboxes = np.array(bboxes, dtype=np.uintc)
+        confs = np.array(confs, dtype=np.float64)
+        labels = np.array(labels, np.uintc)
+
+        if bboxes.ndim == 1:
+            bboxes = bboxes[None, :]
+
+        return (img, bboxes, confs, labels)
+
+
+    def _inference_trt(self) -> tuple([np.ndarray, np.ndarray, np.ndarray]):
+        if self._grabber == 'd3d_gpu':
+            tensor = self._grab_d3d_gpu()
+            ratio = 1.
+            dwdh = torch.tensor([0., 0., 0., 0.], device=0)
+            img = tensor
+        
+        if self._grabber == 'win32':
+            img = self._grab()
+
+        if self._grabber == 'd3d_np':
+            img = self._grab_d3d_np()
+
+        if self._grabber == 'd3d_np' or self._grabber == 'win32':
+            bgr, ratio, dwdh = letterbox(img, (self.section_size, self.section_size))
+            tensor = blob(bgr[:, :, ::-1], return_seg=False)
+            dwdh = torch.asarray(dwdh * 2, dtype=torch.float32, device=0)
+            tensor = torch.asarray(tensor, device=0)
+
+        data = self.model(tensor)
+        bboxes, confs, labels = det_postprocess(data)
+        bboxes -= dwdh
+        bboxes /= ratio
+        
+        if len(bboxes) > 0:
+            bboxes = box_convert(bboxes,'xyxy', 'cxcywh')
+        bboxes =  bboxes.squeeze().cpu().numpy().astype(np.intc)
+        confs = confs.squeeze().cpu().numpy().tolist()
+        labels = labels.squeeze().cpu().numpy().tolist()
+
+        if isinstance(confs, float):
+            confs = [confs]
+        if isinstance(labels, int):
+            labels = [labels]
+
+        confs = np.array(confs, dtype=np.float64)
+        labels = np.array(labels, dtype=np.uintc)
+
+        if bboxes.ndim == 1:
+            bboxes = bboxes[None, :]
+
+        return (img, bboxes, confs, labels)
+
+
+    def _perform_target_selection(self, 
+                                    bboxes: np.ndarray, 
+                                    confs: np.ndarray, 
+                                    labels: np.ndarray, 
+                                    prefer_body: bool) -> tuple[int, int, int, int, int, int, int, int]:
+        conf = float(0)
+        if self.side == 'ct':
+            valid_idcs = np.where(labels>1)[0]
+            bboxes = bboxes[valid_idcs]
+            confs = confs[valid_idcs]
+            labels = labels[valid_idcs]
+
+        if self.side == 't':
+            valid_idcs = np.where(labels<=1)[0]
+            bboxes = bboxes[valid_idcs]
+            confs = confs[valid_idcs]
+            labels = labels[valid_idcs]
+            
+        if labels.size > 1:
+            dist = bboxes[:, 0] - self.section_size // 2
+            right_sided = np.where(dist > 0)[0]
+            if len(right_sided) > 0:
+                dist = dist[right_sided]
+                bboxes = bboxes[right_sided]
+                confs = confs[right_sided]
+                labels = labels[right_sided]             
+            closest = np.argsort(dist)
+
+            if (0 or 2 in labels) and (prefer_body):
+                closest_body_idx = np.where((labels[closest] == 0).any() or (labels[closest] == 2).any())[0][0]
+                target_body_idx = closest[closest_body_idx]
+                target= bboxes[target_body_idx]
+                self.conf = float(confs[target_body_idx])
+            else:
+                target = bboxes[closest[0]]
+                self.conf = float(confs[closest[0]])
+            
+        elif labels.size == 1:
+            target = bboxes
+            conf = float(confs)
+
+        if labels.size > 0:
+            cx, cy, w, h = np.squeeze(target)
+            x1, x2 = (cx - w//2 - self.section_size // 2, cx + w//2 - self.section_size // 2)
+            y1, y2 = (cy - h//2 - self.section_size // 2, cx + h//2  - self.section_size // 2)
+            dx = int(cx-self.section_size//2)
+            dy = int(cy-self.section_size//2)
+
+            return (True, conf, x1, y1, x2, y2, w, h, dx, dy)
+        else:
+            return (False, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+
+    def _smooth_linear_aim(self, dx: int, dy: int, xyxywh: list, sensitivity: int) -> None:
+            x1, y1, x2, y2, w, h = xyxywh
+            n_steps = abs(dx//4) if abs(dx) > 100 else abs(dx//6)
+            if n_steps <= 3:
+                n_steps = 2
+            for _ in range(n_steps):
+                smooth = 2 * n_steps * sensitivity
+                if (x1+w//4 > 0) or (x2-w//4 < 0) or (y1-h//8 > 0) or (y2+h//8 < 0):
+                    pyautogui.move(int(dx//smooth/MOUSE_X_MOVE_DAMPING), int(dy//smooth/MOUSE_Y_MOVE_DAMPING), 0, _pause=False)
+                _ = accurate_timing(sensitivity)
+    
+
+    def _visualize(self, frame: np.ndarray, 
+                         bboxes: Union[np.ndarray, list], 
+                         confs: Union[np.ndarray, list], 
+                         labels: Union[np.ndarray, list], 
+                         min_conf: float) -> np.ndarray:
+
+        if not isinstance(frame, np.ndarray):
+            frame = frame.squeeze().permute(1, 2, 0).cpu().numpy() * 255
+        frame = np.ascontiguousarray(frame, dtype=np.uint8)
         for bbox, conf, label in zip(bboxes, confs, labels):
             cx, cy, w, h = bbox
             conf = round(conf, 2)
-            if conf > minconf:
+            if conf > min_conf:
                 if label == 0 or label == 1:
                     cv2.rectangle(frame, (cx-w//2, cy-h//2), (cx+w//2, cy+h//2), color=(255, 255, 0), thickness=2)
                     cv2.putText(frame, str(conf), (cx, cy), color=(255, 255, 9), fontScale=1, fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, thickness=2)
@@ -154,3 +277,60 @@ class AimAide():
                     cv2.putText(frame, str(conf), (cx, cy), color=(255, 0, 255), fontScale=1, fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, thickness=2)
 
         return frame
+
+
+    def _benchmark(self, infer_method: str) -> None:
+        t1 = time.perf_counter()
+        while time.perf_counter() - t1 < 5:
+            t = time.perf_counter() - t1
+            print(f'Running in benchmark mode for 300 iterations in {(4-t):.1f} sec!', end='\r', flush=True)
+            time.sleep(1)
+        print('\n')
+        self.run(infer_method, min_conf=.8, visualize=False, prefer_body=False, sensitivity=1, view_only=True, benchmark=True)
+
+
+    def run(self, infer_method: str, min_conf: float, sensitivity: int, visualize: bool, prefer_body: bool, view_only: bool, benchmark: bool) -> None:
+        self._switch_side(self.side)
+        self.listener_switch = Thread(target=self.user_switch_side, daemon=True)
+        self.listener_switch.start()
+        
+        max_dist = np.hypot(self.section_size, self.section_size)
+        conf = 0
+        count_fps, count = 0, 0
+        dx, dy = 0, 0
+
+        if infer_method == 'trt':
+            _infer_func = self._inference_trt
+        if infer_method == 'yolo':
+            _infer_func = self._inference_yolo
+
+        avg_fps = []
+        while True:
+            runtime_start = time.perf_counter()
+            img, bboxes, confs, labels = _infer_func()
+            detected, conf, x1, y1, x2, y2, w, h, dx, dy = self._perform_target_selection(bboxes, confs, labels, prefer_body=prefer_body)
+
+            if detected and conf > min_conf and np.hypot(dx, dy) < max_dist and not view_only:
+                self._smooth_linear_aim(dx, dy, [x1, y1, x2, y2, w, h], sensitivity)
+
+            runtime_end = time.perf_counter()        
+            if count_fps == 30:
+                print(f'[{self._side_color}]FPS:{(int(1/(runtime_end-runtime_start)))} | conf:{conf:.2f} dx:{abs(dx):03d} dy:{abs(dy):03d}' , end='\r', flush=True)
+                count_fps = 0
+            else:
+                count_fps += 1
+
+            if visualize:
+                plot = self._visualize(img, bboxes, confs, labels, min_conf)
+                cv2.imshow('cap', plot)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+            if benchmark:
+                count += 1
+                avg_fps.append(int(1/(runtime_end-runtime_start)))
+                if count > 300:
+                    print(f'Average FPS: {np.mean(np.array(avg_fps)):.2f} (dev:{np.std(np.array(avg_fps)):.2f})                       _')
+                    print('Above 60 FPS increase sensitivity if aim aiding feels unnatural (-sensitivity argument: default 1).')
+                    break
+        sys.exit()
